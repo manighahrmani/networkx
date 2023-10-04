55bda62cdf1bce1d249b395fdbc1be634db05514
manighahrmani <mashapicasso@gmail.com>
2023-10-04 16:09:06 +0100

    Generate fill edges by calling the solver

--- src/mfi.py
+++ src/mfi.py
@@ -1,87 +1,118 @@
 """
-mfi.py: Module for finding the Minimum Fill-In (MFI) of a graph.
-
-This module contains a function that calculates the minimum triangulation
-of a given graph using a brute-force approach. It also includes test cases
-for the function.
+Minimum Fill-In Module
+---------------------
+This module contains functions for generating a grid graph,
+running a minimum fill-in solver, and creating a triangulated graph based on
+the solver's output.
+It includes the following functions:
+- generate_grid_graph
+- run_solver
+- generate_and_solve
 """
 
-import unittest
-import itertools
+from typing import List, Tuple
+import subprocess
+import os
 import networkx as nx  # type: ignore
+import matplotlib.pyplot as plt  # type: ignore
 
 
-def minimum_triangulation_bruteforce(graph):
+def generate_grid_graph(num_rows: int, num_columns: int) -> nx.Graph:
     """
-    Find the minimum triangulation of a graph using a brute-force approach.
+    Generates a 2D grid graph with 'm' rows and 'n' columns.
+    Writes the graph to a text file in a specific format.
 
     Parameters:
-    - graph (NetworkX graph): The input graph.
+        num_rows (int): Number of rows.
+        num_columns (int): Number of columns.
 
     Returns:
-    - NetworkX graph: The triangulated graph.
-    - int: The number of added edges for triangulation.
+        nx.Graph: The generated NetworkX graph.
     """
-    min_added_edges = float('inf')
-    # best_ordering = None
-    best_triangulated_graph = None
 
-    # Generate all possible elimination orderings
-    all_orderings = list(itertools.permutations(graph.nodes()))
+    # Generate the grid graph
+    graph = nx.grid_2d_graph(num_rows, num_columns)
 
-    for ordering in all_orderings:
-        graph_h = graph.copy()
-        added_edges = 0
+    output_path = "C:\\Users\\manig\\Downloads\\PACE2017-min-fill\\graph.txt"
 
-        for node in ordering:
-            neighbors = list(graph_h.neighbors(node))
+    # Open the file to write the edges
+    with open(output_path, mode='w', encoding="utf-8") as file:
+        for edge in graph.edges():
+            vertex_u, vertex_v = (edge[0][0] + 1) * 10 + edge[0][1] + \
+                1, (edge[1][0] + 1) * 10 + edge[1][1] + 1
+            file.write(f"{vertex_u} {vertex_v}\n")
 
-            # Add edges to make the neighborhood of the node a clique
-            for i, vertex_u in enumerate(neighbors):
-                for j, vertex_v in enumerate(neighbors):
-                    if i < j and not graph_h.has_edge(vertex_u, vertex_v):
-                        graph_h.add_edge(vertex_u, vertex_v)
-                        added_edges += 1
-
-            # Remove the node
-            graph_h.remove_node(node)
-
-        if added_edges < min_added_edges:
-            min_added_edges = added_edges
-            # best_ordering = ordering
-            best_triangulated_graph = graph_h.copy()
-
-    return best_triangulated_graph, min_added_edges
+    return graph
 
 
-MFI_FUNCTION = minimum_triangulation_bruteforce
+def run_solver() -> List[Tuple[int, int]]:
+    """
+    Executes a batch file to run a minimum fill-in solver.
+    Reads the solver's output and returns the fill edges.
+
+    Returns:
+        List[Tuple[int, int]]: List of fill edges.
+    """
+    # Define the path where run_solver.bat and graph.txt are located
+    solver_path = "C:\\Users\\manig\\Downloads\\PACE2017-min-fill"
+
+    # Execute the batch file
+    subprocess.run(os.path.join(solver_path, "run_solver.bat"),
+                   shell=True, cwd=solver_path, check=True)
+
+    # Read the output and count lines
+    fill_edges = []
+    with open(os.path.join(solver_path, "output.txt"), mode="r", encoding="utf-8") as file:
+        lines = file.readlines()
+        num_lines = len(lines)
+        fill_edges = [(int(line.split()[0]), int(line.split()[1]))
+                      for line in lines]
+
+    print(f"Number of added edges: {num_lines}")
+
+    return fill_edges
 
 
-class TestMinimumTriangulation(unittest.TestCase):
-    """Test cases for minimum triangulation functions."""
+def generate_and_solve(num_rows: int, num_columns: int) -> Tuple[nx.Graph, List[Tuple[int, int]]]:
+    """
+    Calls `generate_grid_graph` to generate a graph and write it to a text file.
+    Then, calls `run_solver` to find and return the fill edges for graph triangulation.
 
-    def test_minimum_triangulation(self):
-        """Test the minimum_triangulation_bruteforce function."""
-        # 3x3 grid graph
-        graph = nx.grid_2d_graph(3, 3)
-        _, added_edges = MFI_FUNCTION(graph)
-        self.assertEqual(added_edges, 5)
+    Parameters:
+        num_rows (int): Number of rows.
+        num_columns (int): Number of columns.
 
-        # 3x4 grid graph
-        graph = nx.grid_2d_graph(3, 4)
-        _, added_edges = MFI_FUNCTION(graph)
-        self.assertEqual(added_edges, 9)  # 5 + 4 * (4 - 3)
+    Returns:
+        Tuple[nx.Graph, List[Tuple[int, int]]]: Original graph and list of fill edges.
+    """
 
-        # 2x2 grid graph
-        graph = nx.grid_2d_graph(2, 2)
-        _, added_edges = MFI_FUNCTION(graph)
-        self.assertEqual(added_edges, 5)  # 4 * 2 - 3
+    # Generate grid graph and save to graph.txt
+    graph = generate_grid_graph(num_rows, num_columns)
 
-        # 2x3 grid graph
-        graph = nx.grid_2d_graph(2, 3)
-        _, added_edges = MFI_FUNCTION(graph)
-        self.assertEqual(added_edges, 9)  # 4 * 3 - 3
+    # Run the solver and get fill edges
+    fill_edges = run_solver()
+
+    return graph, fill_edges
 
 
+# Example usage
 if __name__ == "__main__":
-    unittest.main()
+    rows, columns = 5, 5
+    grid, chords = generate_and_solve(rows, columns)
+    print("The edges of the original graph:", grid.edges())
+
+    # Plot and save the original graph
+    plt.figure(figsize=(8, 6))
+    nx.draw(grid, with_labels=True, font_weight='bold')
+    plt.savefig('3x4_grid.png')
+
+    print("Fill edges:", chords)
+
+    # Create the triangulated graph
+    graph_triangulated = grid.copy()
+    graph_triangulated.add_edges_from(chords)
+
+    # Plot and save the triangulated graph
+    plt.figure(figsize=(8, 6))
+    nx.draw(graph_triangulated, with_labels=True, font_weight='bold')
+    plt.savefig('3x4_grid_triangulated.png')
