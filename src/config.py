0a90d582ff49fe8958b5b18968c686cebff56827
manighahrmani <mashapicasso@gmail.com>
2023-10-05 01:51:19 +0100

    Choose path based on OS

--- src/config.py
+++ src/config.py
@@ -1,3 +1,14 @@
-SOLVER_PATH = "C:\\Users\\manig\\Downloads\\PACE2017-min-fill"
+import platform
+
+# Determine the operating system
+os_type = platform.system()
+
+if os_type == "Windows":
+    SOLVER_PATH = "C:\\Users\\manig\\Downloads\\PACE2017-min-fill"
+elif os_type == "Darwin":  # Darwin is the operating system for MacOS
+    SOLVER_PATH = "/Users/manigh/Desktop/PACE2017-min-fill"
+else:
+    raise EnvironmentError("Unsupported operating system")
+
 ROWS = 11
 MAX_COLUMNS = 12
