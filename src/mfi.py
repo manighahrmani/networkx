6e14ee0d5ea2128813ad9dbab1ad011e7451f02d
manighahrmani <mashapicasso@gmail.com>
2023-10-11 03:37:53 +0100

    Move mfi2 to mfi

--- src/mfi.py
+++ src/mfi.py
@@ -1,17 +1,8 @@
 """
 Minimum Fill-In Module
----------------------
-This module contains functions for generating a grid graph,
-running a minimum fill-in solver, and creating a triangulated graph based on
-the solver's output.
-It includes the following functions:
-- generate_grid_graph
-- run_solver
-- generate_and_solve
 """
 
 import csv
-from typing import List, Tuple
 import subprocess
 import os
 import matplotlib.pyplot as plt  # type: ignore
@@ -21,135 +12,134 @@ from config import SOLVER_PATH, ROWS, MAX_COLUMNS
 
 def generate_grid_graph(num_rows: int, num_columns: int) -> nx.Graph:
     """
-    Generates a 2D grid graph with 'm' rows and 'n' columns.
-    Writes the graph to a text file in a specific format.
+    Generate a grid graph with custom vertex labels and save its edges to a text file.
 
     Parameters:
-        num_rows (int): Number of rows.
-        num_columns (int): Number of columns.
+    - num_rows (int): The number of rows in the grid.
+    - num_columns (int): The number of columns in the grid.
 
     Returns:
-        nx.Graph: The generated NetworkX graph.
+    - nx.Graph: The generated graph with custom vertex labels.
+
+    The vertices of the generated graph are labeled as strings, starting with '1' 
+    followed by two digits for the row number and two digits for the column number.
+    For example, the vertex in the first row and first column is labeled '10101'.
+
+    The edges of the graph are saved in a text file.
     """
 
-    # Generate the grid graph
+    # Generate the original grid graph
     graph = nx.grid_2d_graph(num_rows, num_columns)
 
+    # Generate a mapping from old labels (tuples) to new labels (strings).
+    # Add a leading '1' to each label to avoid leading zeros.
+    mapping = {(r, c): f"1{r+1:02}{c+1:02}" for r in range(num_rows)
+               for c in range(num_columns)}
+
+    # Create a new graph with nodes relabeled
+    relabeled_graph = nx.relabel_nodes(graph, mapping)
+
+    # Generate the output path and write the edges to the file
     output_path = os.path.join(SOLVER_PATH, "graph.txt")
+    with open(output_path, mode='w', encoding='utf8') as f:
+        for edge in relabeled_graph.edges():
+            f.write(f"{edge[0]} {edge[1]}\n")
+    # Also save it to the logs folder
+    with open(
+        os.path.join("logs", f'{num_rows}x{num_columns}.txt'),
+        mode='w',
+        encoding='utf8'
+    ) as f:
+        for edge in relabeled_graph.edges():
+            f.write(f"{edge[0]} {edge[1]}\n")
 
-    # Represent the vertices in 4 digits (first 2 digits for row, last 2 for column)
-    with open(output_path, mode='w', encoding="utf-8") as file:
-        for edge in graph.edges():
-            vertex_u = f"{edge[0][0]:02}{edge[0][1]:02}"
-            vertex_v = f"{edge[1][0]:02}{edge[1][1]:02}"
-            file.write(f"{vertex_u} {vertex_v}\n")
+    # Generate the positions for the nodes.
+    # In this case, we can simply reverse the labels back to tuples for positioning.
+    # Reversed to match typical Cartesian coordinates
+    pos = {mapping[key]: (key[1], -key[0]) for key in mapping.keys()}
 
-    return graph
+    return relabeled_graph
 
 
-def run_solver() -> List[Tuple[Tuple[int, int], Tuple[int, int]]]:
+def run_solver():
+    """
+    Run an external solver to generate fill edges that triangulate the graph.
+
+    Returns:
+    - List[Tuple[str, str]]: The list of fill edges added to triangulate the graph.
+
+    This function runs an external solver script that reads the graph from a text file,
+    triangulates the graph, and then writes the fill edges to an output text file.
+    The function reads this output file and returns the fill edges as a list of tuples.
+    """
+
     os_type = os.name
     script_filename = "run_solver.bat" if os_type == "nt" else "run_solver.sh"
 
-    print(f"Running {script_filename}...")
     subprocess.run(os.path.join(SOLVER_PATH, script_filename),
                    shell=True, cwd=SOLVER_PATH, check=True)
 
-    print("Done!")
     fill_edges = []
     with open(os.path.join(SOLVER_PATH, "output.txt"), mode="r", encoding="utf-8") as file:
         lines = file.readlines()
-
         for line in lines:
-            vertex_1, vertex_2 = map(int, line.split())
-            row_1, col_1 = divmod(vertex_1, 100)
-            row_2, col_2 = divmod(vertex_2, 100)
-            fill_edges.append(((row_1, col_1), (row_2, col_2)))
+            # Remove any leading/trailing white spaces and split the vertices
+            vertices = line.strip().split(" ")
+
+            # Add the edge as a tuple to the fill_edges list
+            if len(vertices) == 2:
+                fill_edges.append((vertices[0], vertices[1]))
 
     return fill_edges
 
 
-def generate_and_solve(
-    num_rows: int,
-    num_columns: int
-) -> Tuple[nx.Graph, list[tuple[tuple[int, int], tuple[int, int]]]]:
+def generate_triangulated_grid_graph(num_rows: int, num_columns: int):
     """
-    Calls `generate_grid_graph` to generate a graph and write it to a text file.
-    Then, calls `run_solver` to find and return the fill edges for graph triangulation.
+    Generate a grid graph, triangulate it, and visualize the original and triangulated graphs.
 
     Parameters:
-        num_rows (int): Number of rows.
-        num_columns (int): Number of columns.
+    - num_rows (int): The number of rows in the grid.
+    - num_columns (int): The number of columns in the grid.
 
     Returns:
-        Tuple[nx.Graph, List[Tuple[int, int]]]: Original graph and list of fill edges.
+    - Tuple[nx.Graph, List[Tuple[str, str]], nx.Graph, List[str]]:
+        * The original grid graph.
+        * The fill edges added to triangulate the graph.
+        * The triangulated graph.
+        * The largest clique in the triangulated graph.
+
+    The function saves images of both the original and triangulated graphs.
+    The vertices in the largest clique of the triangulated graph are colored differently.
     """
 
-    # Generate grid graph and save to graph.txt
-    graph: nx.Graph = generate_grid_graph(num_rows, num_columns)
+    os.makedirs(os.path.join('images', 'original'), exist_ok=True)
+    os.makedirs(os.path.join('images', 'triangulated'), exist_ok=True)
 
-    # Run the solver and get fill edges
-    fill_edges: List[Tuple[Tuple[int, int], Tuple[int, int]]] = []
-    print("Running solver...")
-    fill_edges = run_solver()
-    print("Edges:", graph.edges())
-    print("Fill edges:", fill_edges)
-
-    return graph, fill_edges
-
-
-def generate_triangulated_grid_graph(
-        num_rows: int,
-        num_columns: int
-) -> Tuple[
-        nx.Graph,
-        List[Tuple[Tuple[int, int], Tuple[int, int]]],
-        nx.Graph,
-        List[Tuple[int, int]]
-]:
-    """
-    Generates a triangulated grid graph, plots and saves it.
-    Highlights the largest clique in the triangulated graph.
-
-    Parameters:
-        num_rows (int): The number of rows in the grid graph.
-        num_columns (int): The number of columns in the grid graph.
-
-    Returns:
-        grid (nx.Graph): The original grid graph.
-        chords (List[Tuple[Tuple[int, int], Tuple[int, int]]]): The fill edges.
-        graph_triangulated (nx.Graph): The triangulated graph.
-        largest_clique (List[Tuple[int, int]]): The largest clique in the triangulated graph.
-
-    Raises:
-        RuntimeError: If the graph is not triangulated.
-    """
-    # Generate the grid graph and its fill edges
-    grid: nx.Graph = None
-    chords: List[Tuple[Tuple[int, int], Tuple[int, int]]] = []
-    grid, chords = generate_and_solve(
-        num_rows=num_rows, num_columns=num_columns)
+    # Generate the grid graph (`generate_grid_graph`) and find the fill edges (`run_solver`)
+    grid = generate_grid_graph(num_rows, num_columns)
+    chords = run_solver()
 
     # Get node positions for the original graph
-    # You can use other layout functions if you prefer
-    pos = nx.spring_layout(grid)
+    # This is so that the vertices of grid graph and triangulated graph have the same positions
+    pos = {node: (int(node[3:5]) - 1, -(int(node[1:3]) - 1))
+           for node in grid.nodes()}
 
     # Plot and save the original graph using the positions
     plt.figure(figsize=(8, 6))
     nx.draw(grid, pos, with_labels=True, font_weight='bold')
-    plt.savefig(f'{num_rows}x{num_columns}_grid.png')
+    plt.savefig(os.path.join('images', 'original',
+                f'{num_rows}x{num_columns}_grid.png'))
 
     # Create the triangulated graph
     grid_triangulated: nx.Graph = grid.copy()
-    grid_triangulated.add_edges_from(chords)
+    grid_triangulated.add_edges_from(chords)  # This should work
 
     # Check if the graph is truly chordal (triangulated)
     if not nx.is_chordal(grid_triangulated):
         raise RuntimeError("The graph is not triangulated!")
 
     # Find all cliques
-    cliques: List[List[Tuple[int, int]]] = []
-    cliques = list(nx.find_cliques(grid_triangulated))
+    cliques: List[List[str]] = list(nx.find_cliques(grid_triangulated))
 
     # Find the largest clique
     largest_clique = max(cliques, key=len)
@@ -162,50 +152,50 @@ def generate_triangulated_grid_graph(
     plt.figure(figsize=(8, 6))
     nx.draw(grid_triangulated, pos, with_labels=True,
             font_weight='bold', node_color=node_colors)
-    plt.savefig(f'{num_rows}x{num_columns}_triangulated.png')
+    plt.savefig(os.path.join('images', 'triangulated',
+                f'{num_rows}x{num_columns}_triangulated.png'))
 
     return grid, chords, grid_triangulated, largest_clique
 
 
-def save_grid_and_chords_to_file(num_rows, num_columns, grid, chords, largest_clique):
-    # Create a logs directory if it doesn't exist
-    logs_dir = "./logs"
-    os.makedirs(logs_dir, exist_ok=True)
-
-    output_path = os.path.join(logs_dir, f"{num_rows}x{num_columns}.txt")
-
-    with open(output_path, mode='w', encoding="utf-8") as file:
-        # Write the edges of the grid
-        for edge in grid.edges():
-            vertex_u = f"{edge[0][0]:02}{edge[0][1]:02}"
-            vertex_v = f"{edge[1][0]:02}{edge[1][1]:02}"
-            file.write(f"{vertex_u} {vertex_v}\n")
-
-        # Write the chords
-        file.write("\n" + "="*20 + "\n")
-        for chord in chords:
-            file.write(f"{chord[0]} {chord[1]}\n")
-
-        # Write the largest clique
-        file.write("\n" + "="*20 + "\n")
-        file.write(" ".join(map(str, largest_clique)))
-
-
 def run_experiments() -> None:
+    """
+    Run experiments to generate triangulated grid graphs and collect data.
+
+    Parameters:
+    - ROWS (int): The constant number of rows for the grid.
+    - MAX_COLUMNS (int): The maximum number of columns to iterate through.
+
+    Returns:
+    - None
+
+    This function iterates through a range of columns from `ROWS` to `MAX_COLUMNS`,
+    generating triangulated grid graphs for each. It calculates the treewidth and
+    number of added chords for each grid, saving these data points to a CSV file.
+    Additionally, it writes the added chords and largest clique to a text file for each grid.
+    """
+
     # Initialize the CSV file and write header
-    with open('grid_data.csv', mode='a', newline='', encoding="utf-8") as csvfile:
+    with open('grid_data.csv', mode='w', newline='', encoding="utf-8") as csvfile:
         csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(
+            ['Columns', 'Rows', 'Num_Added_Chords', 'Treewidth'])
 
     for column in range(ROWS, MAX_COLUMNS + 1):
         print(f"Running experiment for {ROWS}x{column} grid...")
 
-        # Replace the following line with your actual function to generate the triangulated grid
-        grid, chords, _, largest_clique = generate_triangulated_grid_graph(
+        # Generate the triangulated grid
+        _, chords, _, largest_clique = generate_triangulated_grid_graph(
             num_rows=ROWS, num_columns=column)
 
-        # Save the grid graph and additional information
-        save_grid_and_chords_to_file(
-            ROWS, column, grid, chords, largest_clique)
+        # Write the added chords and largest clique to a text file
+        with open(os.path.join("logs", f"{ROWS}x{column}.txt"), mode='a', encoding="utf-8") as file:
+            file.write("=" * 20 + "\n")
+            for chord in chords:
+                file.write(f"{chord[0]} {chord[1]}\n")
+            file.write("=" * 20 + "\n")
+            for node in largest_clique:
+                file.write(f"{node}\n")
 
         # Calculate the treewidth and number of added chords
         treewidth = len(largest_clique) - 1
