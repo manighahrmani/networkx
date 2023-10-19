503d1c09564672c8c270276e3c5cb36da1afea69
manighahrmani <mashapicasso@gmail.com>
2023-10-19 01:14:56 +0100

    Add functions to write input graph to solver folder and file

--- src/utility.py
+++ src/utility.py
@@ -15,8 +15,58 @@ It includes the following functions:
 """
 
 from typing import List, Union, Tuple
+import os
 import unittest
 import networkx as nx  # type: ignore
+from src.config import SOLVER_PATH
+
+
+def write_input_graph_to_solver_folder(graph: nx.Graph) -> None:
+    """
+    Write the edges of the input graph to a solver folder.
+
+    Parameters:
+    - relabeled_graph (nx.Graph): The graph with relabeled vertices.
+
+    The function writes the edges of the relabeled graph to a text file.
+    The file is saved in the solver path.
+    Each line in the file represents an edge and contains two vertex labels separated by a space.
+    """
+    output_path = os.path.join(SOLVER_PATH, "graph.txt")
+    with open(output_path, mode='w', encoding='utf8') as f:
+        for edge in graph.edges():
+            f.write(f"{edge[0]} {edge[1]}\n")
+
+
+def write_input_graph_to_file(
+        num_rows: int,
+        num_columns: int,
+        relabeled_graph: nx.Graph, folders: List[str]
+) -> None:
+    """
+    Write the edges of the input graph to a text file.
+
+    Parameters:
+    - num_rows (int): The number of rows in the grid.
+    - num_columns (int): The number of columns in the grid.
+    - relabeled_graph (nx.Graph): The graph with relabeled vertices.
+    - folders (List[str]): The list of folders where the file will be saved.
+
+    The function writes the edges of the relabeled graph to a text file.
+    The name of the file is in the format '{num_rows}x{num_columns}.txt'.
+    The file is saved in the specified folders.
+    Each line in the file represents an edge and contains two vertex labels separated by a space.
+    """
+    # Join the folders to form the path
+    folder_path = os.path.join(*folders)
+
+    with open(
+        os.path.join(folder_path, f'{num_rows}x{num_columns}.txt'),
+        mode='w',
+        encoding='utf8'
+    ) as f:
+        for edge in relabeled_graph.edges():
+            f.write(f"{edge[0]} {edge[1]}\n")
 
 
 def get_vertex_degree(graph: nx.Graph, vertex: Union[int, Tuple[int, int]]) -> int:
@@ -308,7 +358,8 @@ class TestGraphFunctions(unittest.TestCase):
         """Test for get_missing_edges function."""
         graph = nx.complete_graph(5)
         for vertex in graph.nodes:
-            self.assertEqual(get_missing_edges_in_neighborhood(graph, vertex), [])
+            self.assertEqual(
+                get_missing_edges_in_neighborhood(graph, vertex), [])
 
         graph = nx.path_graph(5)
         self.assertEqual(get_missing_edges_in_neighborhood(graph, 0), [])
