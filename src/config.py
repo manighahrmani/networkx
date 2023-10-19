2d396ce64f6c1a1c2254d6c80588837fa1d57227
manighahrmani <mashapicasso@gmail.com>
2023-10-19 01:04:00 +0100

    Add operating system detection and grid configuration to config.py

--- src/config.py
+++ src/config.py
@@ -1,3 +1,23 @@
+"""
+config.py
+
+This module determines the operating system and sets the path to the solver accordingly.
+It also sets the number of rows and maximum number of columns for the grid.
+
+The module uses the platform module to determine the operating system. 
+If the operating system is Windows, the solver path is set to a specific location on the C drive.
+If the operating system is macOS, the solver path is set to a specific location on the desktop.
+If the operating system is neither Windows nor Darwin, an EnvironmentError is raised.
+
+The number of rows and the maximum number of columns for the grid are set to 5 and 15, respectively.
+
+Variables:
+- os_type: The type of the operating system.
+- SOLVER_PATH: The path to the solver.
+- ROWS: The number of rows in the grid.
+- MAX_COLUMNS: The maximum number of columns in the grid.
+"""
+
 import platform
 
 # Determine the operating system
