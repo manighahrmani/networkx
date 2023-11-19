b9ad9b05ebf36ea7075432a175b22717ac0aea39
manighahrmani <mashapicasso@gmail.com>
2023-11-19 02:56:49 +0000

    Delete unused main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-# def save_grid_graph(m, n, filename):
-#     G = nx.grid_2d_graph(m, n)
-#     pos = {(x, y): (y, -x) for x, y in G.nodes()}
-#     nx.draw(G, pos, with_labels=True, node_color='lightblue',
-#             font_weight='bold', node_size=700, font_size=18)
-#     plt.savefig(filename)
